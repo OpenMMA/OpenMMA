da11f442314a7b81cdccfc7814ef92fba3303b6b
Eaorns <emo@endr.nl>
2023-01-15 22:17:32 +0100

    Some basic layout and first version calendar

---
+++ www/index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>OpenCDX - Eureka</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+        <script src="js/calendar.js"></script>
+        <link href="css/style.css" rel="stylesheet">
+        <link href="css/calendar.css" rel="stylesheet">
+    </head>
+    <body class="bg-body vh-100">
+        <nav class="navbar bg-body-tertiary fixed-top">
+            <div class="container">
+                <a class="navbar-brand" href="#">
+                    <img src="eureka_logo.png" alt="Bootstrap" height="62">
+                </a>
+                <p class="fs-5 ms-auto me-3 mb-1">
+                    Harald Haraldsson
+                </p>
+                <div class="dropdown">
+                    <a href="#" class="dropdown-toggle text-decoration-none text-black" data-bs-toggle="dropdown">
+                        <img class="account-dropdown" src="harold.png">
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end mt-2" role="menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Action two</a></li>
+                        <li><a class="dropdown-item" href="#">Action three</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <div class="container-fluid mt-5 pt-3">
+            <div class="row justify-content-center mt-5">
+                <!-- <div class="col-3 h-100">a</div> -->
+                <div class="col-6">
+                    <div class="container calendar-container p-3 sticky-top" id="calendar"></div>
+                </div>
+                <div class="col-4 h-100">
+                    <div class="container">
+
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card m-3 mb-1">
+                            <div class="row g-0">
+                                <div class="col-md-4">
+                                    <img class="img-fluid rounded-start" src="https://sveureka.nl/wp-content/uploads/sites/331/2021/11/Thrilling-chills-and-reading-stories-724x1024.png">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Thrilling Tales and Spooky Stories</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
