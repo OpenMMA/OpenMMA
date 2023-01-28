0187822d5b414a0934dc62e72a54e20e8d79d8bc
Eaorns <emo@endr.nl>
2023-01-28 18:15:45 +0100

    Undid accidental credential commit (oops... no, they're no longer valid)

--- www/includes/settings.php
+++ www/includes/settings.php
@@ -2,10 +2,10 @@
 return (object) array(
     /* Database connection settings */
     'db' => array(
-        'mysql_host'     => 'localhost',
-        'mysql_db'       => 'u69606p66242_opencdx',
-        'mysql_user'     => 'u69606p66242_opencdx',
-        'mysql_pass'     => 'ngDxA5eDHYrJ8XiCn^MV',
+        'mysql_host'     => '',
+        'mysql_db'       => '',
+        'mysql_user'     => '',
+        'mysql_pass'     => '',
         'mysql_charset'  => 'utf8mb4',
 
         /* Session settings */
@@ -14,9 +14,9 @@ return (object) array(
 
     /* Mailer e-mail account settings */
     'mailer' => array(
-        'host'      => 'mail.zxcs.nl',
-        'port'      => 465,
-        'username'  => 'eureka@endr.nl',
-        'password'  => 'opdpa6$8u$v4djjmit#q'
+        'host'      => '',
+        'port'      => 25,
+        'username'  => '',
+        'password'  => ''
     )
 );
