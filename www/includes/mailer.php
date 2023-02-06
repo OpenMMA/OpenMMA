567a46c141201682d2fc38a6b81184a8fb052e91
Eaorns <emo@endr.nl>
2023-02-06 10:59:47 +0100

    Basic mailer class

--- www/includes/mailer.php
+++ www/includes/mailer.php
@@ -13,32 +13,41 @@ class Mailer {
 
     public function __construct() {
         /* Get settings from settings file */
-        $settings = (include("settings.php"))->mailer;
+        $settings = (object)(include("settings.php"))->mailer;
 
-        $this->mailer = new PHPMailer();
+        $this->mailer = new PHPMailer(true);
         $this->mailer->isSMTP();                                  //Send using SMTP
         $this->mailer->SMTPAuth   = true;                         //Enable SMTP authentication
         $this->mailer->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;  //Enable implicit TLS encryption
-        $this->mailer->Host       = $settings["host"];            //Set the SMTP server to send through
-        $this->mailer->Port       = $settings["port"];            
-        $this->mailer->Username   = $settings["username"];        //SMTP username
-        $this->mailer->Password   = $settings["password"];        //SMTP password
+        $this->mailer->Host       = $settings->host;              //Set the SMTP server to send through
+        $this->mailer->Port       = $settings->port;              
+        $this->mailer->Username   = $settings->username;          //SMTP username
+        $this->mailer->Password   = $settings->password;          //SMTP password
     }
 
     public function sendMail($recipient, $subject, $body, $altbody=Null) {
-        $this->mailer->setFrom('eureka@endr.nl', 'Eureka - OpenCDX');
-        $this->mailer->addAddress($recipient);
+        try {
+            echo "a\n";
+            $this->mailer->setFrom('eureka@endr.nl', 'Eureka - OpenCDX');
+            $this->mailer->addAddress($recipient);
 
-        if ($altbody != Null) {
-            $this->mailer->isHTML(true); 
-            $this->mailer->AltBody = $altbody;
-        } else {
-            $this->mailer->isHTML(false); 
+            echo "b\n";
+            
+            if ($altbody != Null) {
+                $this->mailer->isHTML(true); 
+                $this->mailer->AltBody = $altbody;
+            } else {
+                $this->mailer->isHTML(false); 
+            }
+            $this->mailer->Subject = $subject;
+            $this->mailer->Body    = $body;
+            echo "c\n";
+            
+            $this->mailer->send();
+            echo "d\n";
+        } catch (Exception $e) {
+            echo "Message could not be sent. Mailer Error: {$this->mailer->ErrorInfo}";
         }
-        $this->mailer->Subject = $subject;
-        $this->mailer->Body    = $body;
-
-        $this->mailer->send();
     }
 }
 ?>
