013199fc47d05beb51f805c3c56dd70a95f7cafc
Jeroen Tas <jeroentas@live.nl>
2023-03-07 23:43:51 +0100

    added form_submit_right style option

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -7,6 +7,11 @@
     @isset($form_method)
     @method($form_method)
     @endisset
+
+    @if ($form_submit_right ?? false)
+        <div class="d-flex gap-1">
+            <div class="flex-grow-1">
+    @endif
     @foreach ($form_fields as $field)
         @if (is_array($field))
             <div class="container">
@@ -25,5 +30,13 @@
             @include('components.form-field')
         @endif
     @endforeach
+    @if ($form_submit_right ?? false)
+            </div>
+            <div class="mb-3 align-self-end">
+    @endif
     {{ Form::submit($form_submit, ['class' => 'btn btn-primary ' . ($form_submit_classes ?? '')]) }}
+    @if ($form_submit_right ?? false)
+            </div>
+        </div>
+    @endif
 {{ Form::close() }}
--- resources/views/dashboard/system-settings.blade.php
+++ resources/views/dashboard/system-settings.blade.php
@@ -5,6 +5,7 @@
 <div class="pt-3 px-3">
     @include('components.form', ['form_name' => 'site_name_form',
                                  'form_submit' => 'Save',
+                                 'form_submit_right' => true,
                                  'form_target' => '/dashboard/system-settings',
                                  'form_fields' => [
                                     (object)array('type' => 'text', 'name' => 'value', 'required' => true, 'label' => 'Site name', 'default' => app('settings')['site.name']),
