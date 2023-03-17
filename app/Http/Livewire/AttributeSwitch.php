d181e96975f7d2befea9ba870cec7dfa535e3914
Eaorns <emo@endr.nl>
2023-03-17 20:19:29 +0100

    Replaced event registration settings with livewire

---
+++ app/Http/Livewire/AttributeSwitch.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Illuminate\Database\Eloquent\Model;
+use Livewire\Component;
+
+class AttributeSwitch extends Component
+{
+    public Model $model;
+    public string $attribute;
+    public bool $isChecked;
+
+    public function mount()
+    {
+        $this->isChecked = (bool)$this->model->getAttribute($this->attribute);
+    }
+
+    public function updating($field, $value)
+    {
+        $this->model->setAttribute($this->attribute, $value)->save();
+    }
+
+    public function render()
+    {
+        return view('livewire.attribute-switch');
+    }
+}
---
+++ app/Http/Livewire/EventRegistrationSettings.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Event;
+use Livewire\Component;
+
+class EventRegistrationSettings extends Component
+{
+    public Event $event; 
+    public bool $registerable;
+    public bool $enable_comments;
+    public int $max_registrations;
+
+    public function mount()
+    {
+        $this->registerable = (bool)$this->event->registerable;
+        $this->enable_comments = (bool)$this->event->enable_comments;
+    }
+
+    public function updatingRegisterable($registerable)
+    {
+        $this->event->update(['registerable' => $registerable]);
+    }
+
+    public function updatingEnableComments($enable_comments)
+    {
+        $this->event->update(['enable_comments' => $enable_comments]);
+    }
+
+    public function setMaxRegistrations()
+    {
+        $this->event->update(['max_registrations' => $this->max_registrations]);
+    }
+
+    public function render()
+    {
+        return view('livewire.event-registration-settings');
+    }
+}
