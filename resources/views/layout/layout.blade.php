c6015a903c8fdac97f6cc853546e61cdcbd3535a
Jeroen Tas <jeroentas@live.nl>
2023-03-20 23:17:45 +0100

    Add bootstrap css with primary color as purple

--- resources/views/layout/layout.blade.php
+++ resources/views/layout/layout.blade.php
@@ -11,8 +11,8 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         @stack('scripts')
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <link href="{{ asset('/css/bootstrap-openmma.min.css') }}" rel="stylesheet">
         <link href="{{ asset('/css/app.css') }}" rel="stylesheet">
         <script src="{{ asset('/js/app.js') }}"></script>
         @stack('styles')
