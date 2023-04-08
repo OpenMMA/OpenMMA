4f595f8bafdd6e7a8b3855a9df054e1afb9d5b41
Jeroen Tas <jeroentas@live.nl>
2023-04-08 03:30:07 +0200

    Improve table styling

--- resources/views/livewire/user-table.blade.php
+++ resources/views/livewire/user-table.blade.php
@@ -9,106 +9,101 @@
     // dd($roles);
 @endphp
 <div>
-    <div class="d-flex align-items-start mx-2">
-        <div class="flex-grow-1">
-            <input wire:model="query" class="form-control" type="text" placeholder="Search...">
-        </div>
+    <input wire:model="query" class="form-control" type="text" placeholder="Search...">
+    <div class="table-responsive">
+        <table class="table table-striped lw_table" id="user_table">
+            <thead>
+                <tr>
+                    @foreach($cols as $col => $attrs)
+                    <th scope="col">
+                        <div class="d-flex text-nowrap">
+                            {{ $col_opts[$col]['label'] }}
+                            <button wire:click="sortTable('{{ $col }}')" class="btn px-1 py-0">
+                                @switch($attrs['sort_direction'])
+                                    @case('asc')
+                                        <i class="fa-solid fa-sort-up align-end"></i>
+                                        @break
+                                    @case('desc')
+                                        <i class="fa-solid fa-sort-down align-end"></i>
+                                        @break
+                                    @default
+                                        <i class="fa-solid fa-sort align-end"></i>
+                                @endswitch
+                            </button>
+                        </div>
+                    </th>
+                    @endforeach
+                </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @foreach ($users as $user)
+                <tr>
+                    @foreach ($cols as $col => $attrs)
+                    <td>
+                        @switch($col_opts[$col]['display'])
+                            @case('check')
+                                @if($user->$col)
+                                    <i class="fa-solid fa-check text-success"></i>
+                                    @else
+                                    <i class="fa-solid fa-xmark text-danger"></i>
+                                    @endif
+                                @break
+                            @case('date')
+                                {{ Carbon::parse($user->$col)->format("d-m-Y") }}
+                                @break
+                            @case('groups')
+                                @foreach ($user->$col as $group)
+                                    <span class="badge rounded-pill" style="background-color: {{ $groups->firstWhere('id', $group->group)->hexColor }}">
+                                        {{ $group->title }}
+                                    </span><br>
+                                @endforeach
+                                @break
+                            @case('roles')
+                                @foreach ($user->role_ids as $role_id)
+                                    @if ($roles->has($role_id))
+                                    <span class="badge rounded-pill" style="background-color: #ed036f">
+                                        {{ $roles[$role_id]->title }}
+                                    </span><br>
+                                    @endif
+                                @endforeach
+                                @break;
+                            @case('verify')
+                                @livewire('verify-user', ['user' => $user], key($user->id))
+                                @break
+                            @case('val')
+                            @default
+                                {{ $user->$col }}
+                        @endswitch
+                    @endforeach
+                    <td class="hide"><a href="{{ url("/dashboard/user/$user->id") }}" class="btn btn-primary px-1 py-0">View</a></td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
-    <table class="table table-striped lw_table" id="user_table">
-        <thead>
-            {{-- TODO fix table column width to be minimal but not less than header width --}}
-            <tr>
-                @foreach($cols as $col => $attrs)
-                <th scope="col">
-                    <div>
-                        {{ $col_opts[$col]['label'] }}
-                        <button wire:click="sortTable('{{ $col }}')" class="btn px-1 py-0">
-                            @switch($attrs['sort_direction'])
-                                @case('asc')
-                                    <i class="fa-solid fa-sort-up align-end"></i>
-                                    @break
-                                @case('desc')
-                                    <i class="fa-solid fa-sort-down align-end"></i>
-                                    @break
-                                @default
-                                    <i class="fa-solid fa-sort align-end"></i>
-                            @endswitch
-                        </button>
-                    </div>
-                </th>
-                @endforeach
-            </tr>
-        </thead>
-        <tbody class="table-group-divider">
-            @foreach ($users as $user)
-            <tr>
-                @foreach ($cols as $col => $attrs)
-                <td>
-                    @switch($col_opts[$col]['display'])
-                        @case('check')
-                            @if($user->$col)
-                                <i class="fa-solid fa-check text-success"></i>
-                                @else
-                                <i class="fa-solid fa-xmark text-danger"></i>
-                                @endif
-                            @break
-                        @case('date')
-                            {{ Carbon::parse($user->$col)->format("d-m-Y") }}
-                            @break
-                        @case('groups')
-                            @foreach ($user->$col as $group)
-                                <span class="badge rounded-pill" style="background-color: {{ $groups->firstWhere('id', $group->group)->hexColor }}">
-                                    {{ $group->title }}
-                                </span><br>
-                            @endforeach
-                            @break
-                        @case('roles')
-                            @foreach ($user->role_ids as $role_id)
-                                @if ($roles->has($role_id))
-                                <span class="badge rounded-pill" style="background-color: #ed036f">
-                                    {{ $roles[$role_id]->title }}
-                                </span><br>
-                                @endif
-                            @endforeach
-                            @break;
-                        @case('verify')
-                            @livewire('verify-user', ['user' => $user], key($user->id))
-                            @break
-                        @case('val')
-                        @default
-                            {{ $user->$col }}
-                    @endswitch
-                @endforeach
-                <td class="hide"><a href="{{ url("/dashboard/user/$user->id") }}" class="btn btn-primary px-1 py-0">View</a></td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    <div class="flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
-        <div>
-            <p class="small text-muted">
-                Showing
-                <span class="fw-semibold">{{ $users->firstItem() }}</span>
-                to
-                <span class="fw-semibold">{{ $users->lastItem() }}</span>
-                {{-- TODO sometimes 'of' dissapears??? --}}
-                of
-                <span class="fw-semibold">{{ $users->total() }}</span>
-                results
-            </p>
+    <div class="d-flex align-items-center justify-content-between">
+        <div class="small text-muted mb-3">
+            Showing
+            <span class="fw-semibold">{{ $users->firstItem() }}</span>
+            to
+            <span class="fw-semibold">{{ $users->lastItem() }}</span>
+            {{-- TODO sometimes 'of' dissapears??? --}}
+            of
+            <span class="fw-semibold">{{ $users->total() }}</span>
+            results
         </div>
-        <div class="d-sm-flex">
-            <div class="d-sm-flex flex-row align-items-start pe-3">
-                <p class="small text-muted text-nowrap pe-2">
-                    Items per page:
-                </p>
+        <div>
+            <div class="d-inline-block small text-muted text-nowrap pe-1">
+                Items per page:
+            </div>
+            <div class="d-inline-block me-2 mb-3">
                 <select wire:model="entries_per_page" class="form-select form-select-sm">
                     <option value="10">10</option>
                     <option value="25">25</option>
                     <option value="100">100</option>
                 </select>
             </div>
-            <div>
+            <div class="d-inline-block">
                 {{ $users->links() }}
             </div>
         </div>
