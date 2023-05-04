1af2114bf62c6231f93d46270a11912b84369aa7
Jeroen Tas <jeroentas@live.nl>
2023-05-04 15:55:13 +0200

    Fix padding

--- resources/views/profile/profile.blade.php
+++ resources/views/profile/profile.blade.php
@@ -3,11 +3,9 @@
 @extends('profile.layout')
 
 @section('profile.content')
-<div class="p-3">
-    <h2>Profile</h2>
-    <hr class="border-secondary">
-    <div style="max-width: 500px;">
-        @livewire('profile.profile')
-    </div>
+<h2>Profile</h2>
+<hr class="border-secondary">
+<div style="max-width: 500px;">
+    @livewire('profile.profile')
 </div>
 @endsection
