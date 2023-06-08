a7a1dba6c037482921b6842622eff58441168f82
Eaorns <emo@endr.nl>
2023-06-08 19:40:59 +0200

    Semi-custom email message when registering

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,9 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,6 +27,13 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        VerifyEmail::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('Verify Email Address')
+                ->greeting('Hello ' . $notifiable->name . '!')
+                ->line('Thank you for creating an account with ' . setting('site.name') . '. Please click the button below to verify your email address and activate your account.')
+                ->action('Verify Email Address', $url)
+                ->salutation(setting('site.name'));
+        });
     }
 }
