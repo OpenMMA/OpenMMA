109431d69ea584257b0633e0482590131769ac84
Eaorns <emo@endr.nl>
2023-06-15 11:25:47 +0200

    Add warning when not logged in

--- resources/views/events/calendar.blade.php
+++ resources/views/events/calendar.blade.php
@@ -9,6 +9,15 @@
 
 @section('content')
 
+@guest
+<div class="my-2">
+    <div class="alert alert-warning alert-dismissible fade show w-50 m-auto" role="alert">
+        <strong>You are not logged in!</strong> This means you may not be able to see all planned activities.
+        <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+    </div>
+</div>
+@endguest
+
 @livewire('calendar')
 
 <div style="height: 400px;"></div>
