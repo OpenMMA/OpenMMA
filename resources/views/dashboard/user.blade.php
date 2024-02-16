655e4bdf06357133610dde36769c044ac2460df7
Eaorns <emo@endr.nl>
2024-02-16 16:35:15 +0100

    Bugfix: accessed object as array.

--- resources/views/dashboard/user.blade.php
+++ resources/views/dashboard/user.blade.php
@@ -7,23 +7,23 @@
 <p>{{ $user->email }} - Email {{ $user->email_verified_at ? '' : 'not' }} verified.</p>
 <p>Registered at {{ $user->created_at }}</p>
 <h4>Other data</h4>
-    @foreach(setting('account.custom_fields') as $data_fields)
-        @if ($data_fields->type == 'select')
-            <p>{{ $data_fields->label }}:
-            @if ($data_fields->multiple)
-                @foreach($user->custom_data[$data_fields->name] as $di)
-                    {{ $data_fields->options->$di ?? $di }}
+    @foreach(setting('account.custom_fields') as $data_field)
+        @if ($data_field->type == 'select')
+            <p>{{ $data_field->label }}:
+            @if ($data_field->multiple)
+                @foreach($user->custom_data[$data_field->name] as $di)
+                    {{ $data_field->options->$di ?? $di }}
                     @if (!$loop->last)
                         ,
                     @endif
                 @endforeach
             @else
-                {{ $data_fields->options[$di] }}
+                {{ $data_field->options->$di }}
             @endif
 
             </p>
         @else
-            <p>{{ $data_fields->label }}: {{ $user->custom_data[$data_fields->name] }}</p>
+            <p>{{ $data_field->label }}: {{ $user->custom_data[$data_field->name] }}</p>
         @endif
     @endforeach
 <h4>Groups</h4>
